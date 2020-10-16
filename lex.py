aadf7fb99d885140376dc69b2c4c3f42877a5152
Gabriela Pivetta <gpivetta99@gmail.com>
2020-10-15 22:42:33 -0400

    lispy lexer function fill out

--- lex.py
+++ lex.py
@@ -13,4 +13,25 @@ def lex(code: str) -> Iterable[Token]:
     da string de código fornecida.
     """
 
+    token_specification = [
+        ('NUMBER', r'(\+|\-)?\d+(\.\d*)?'),
+        ('STRING', r'\".*\"'),
+        ('BOOL', r'#[t|f]'), 
+        ('CHAR', r'#\\[a-zA-Z]*'),
+        ('QUOTE', r'\''),
+        ('LPAR', r'\('), #open parenthesis
+        ('RPAR', r'\)'), #close parenthesis
+        ('NAME', r'([^;()\"\#\n ]+)'),
+    ]
+
+    # Remove comments from entry
+    code = re.sub(r';;.*', '', code)
+    
+    token_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
+
+    for mo in re.finditer(token_regex, code):
+        kind = mo.lastgroup
+        value = mo.group()
+        yield Token (kind, value)
+
     return [Token('INVALIDA', 'valor inválido')]
